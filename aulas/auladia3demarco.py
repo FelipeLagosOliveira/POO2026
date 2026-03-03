f6fa4235170090e036468f13ee32c1945debfb8c
unknown <20241pvai@estudantes.ifpr.edu.br>
2026-03-03 09:57:53 -0300

    while,for e vetores

---
+++ aulas/auladia3demarco.py
@@ -0,0 +1,26 @@
+import random
+"""user = int(input("digite um valor entre 10 e 15"))
+while (user<10 or user>15):
+    print("Digite um número dentro do intervalo")
+    user = int(input("digite um valor entre 10 e 15"))
+
+valor = random.randint(10,15)
+print(valor)
+if(user == valor):
+    print("Você acertou")
+else:
+    print("Você errou")
+
+valor = random.randint(10,20)
+
+user = int(input("Chute o número que foi sorteado"))
+while (user != valor):
+    print("Valor invalido")
+    user = int(input("Chute o número que foi sorteado"))
+
+print("Você acertou")
+"""
+for i in range (1,11):
+    print(i)
+
+    numeros = [0] * 10
